c41666d35fc7ebe9c4d9d22669a0e853f8c10537
Karl Rydberg <kalle.rydberg02@gmail.com>
2025-10-06 21:55:27 +0200

    Commenting code

--- lab_1.py
+++ lab_1.py
@@ -1,24 +1,23 @@
-import matplotlib.pyplot as plt
-
-
 def nucleobase_count(data_file): 
     """ The purpose of this function is to count the occurences of nucleobases in one or more sequences of DNA. It is intended to 
     take the path of a text file as an argument when called, but the contained data need to be formatted in the same way as the 
-    data in the sample files, with each sequence separated by '>seq'. Mandates an import of matplotlib.pyplot as plt."""
+    data in the sample files, with each sequence separated by '>seq'. Mandates an install of matplotlib. """
 
-    index_counter = 0  # Value placer used to refer to individual elements in list, first [0] to last 
+    import matplotlib.pyplot as plt
+
+    index_counter = 0  # Value placer used to refer to individual list elements
 
     with open(data_file, "r") as file:
         read_file = file.read() 
 
-    sequence_list = read_file.lower().strip().split(">seq") # Cleans string data, splits each sequence into list elements
+    sequence_list = read_file.lower().strip().split(">seq") # Cleans string data and splits each sequence into separate list elements
     sequence_list = [sequence for sequence in sequence_list if sequence] # Removes falsy values, i.e empty list elements
                  
-    for list_iterator in range(len(sequence_list)): # Iterates as many times as list contains elements
+    for list_iterator in range(len(sequence_list)): # Iterates code block as many times as list contains elements
         empty_dictionary = {"a" : 0, "t" : 0, "c" : 0, "g" : 0}
         sequence = sequence_list[index_counter]
-        for nucleobase in sequence: # Nested loop, iterates through a single list element
-            if nucleobase == "a": # Conditions which will update dict values based on nucleobase
+        for nucleobase in sequence: # Iterates through the element which index equals the value of index_counter
+            if nucleobase == "a": 
                 empty_dictionary["a"] += 1
             elif nucleobase == "t":
                 empty_dictionary["t"] += 1
@@ -28,13 +27,19 @@ def nucleobase_count(data_file):
                 empty_dictionary["g"] += 1
             else:
                 continue
-        letters = list(empty_dictionary.keys())
-        frequency = list(empty_dictionary.values())
-        plt.bar(range(len(empty_dictionary)), frequency, tick_label=letters)
+        
+        """ The following block graphs and showcases the data for each sequence contained. Loosely based on concepts found 
+        here: https://www.tutorialspoint.com/plot-a-bar-using-matplotlib-using-a-dictionary """
+        letters = list(empty_dictionary.keys()) # Creates list variable with dict keys as elements
+        frequency = list(empty_dictionary.values()) # Creates list variable with dict values as elements
+        plt.bar(range(len(empty_dictionary)), 
+                frequency, 
+                tick_label=letters)
+        
         plt.title(f"Sequence: {index_counter + 1}")
         plt.xlabel("Nucleobases")
         plt.ylabel("Occurences")
         plt.show()
-        index_counter = index_counter + 1
+        index_counter = index_counter + 1 # Changes the value of index_counter and thusly the referred list element
 
-nucleobase_count(r"C:\Users\kalle\Documents\Github\lab_1_karl_rydberg_de25\data\dna_raw.txt")
+nucleobase_count(r"")
