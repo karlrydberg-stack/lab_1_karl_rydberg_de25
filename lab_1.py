d1d485f6d1f811fd7eadd4bcd6b00fcc14e7696d
Karl Rydberg <kalle.rydberg02@gmail.com>
2025-10-15 21:41:36 +0200

    Added save function

--- lab_1.py
+++ lab_1.py
@@ -6,7 +6,7 @@ def nucleobase_count(data_file):
     import matplotlib.pyplot as plt
 
     index_counter = 0  # Variable used to refer to individual list elements
-
+    
     with open(data_file, "r") as file:
         read_file = file.read() 
 
@@ -17,24 +17,22 @@ def nucleobase_count(data_file):
         empty_dictionary = {"a" : 0, "t" : 0, "c" : 0, "g" : 0}
         sequence = sequence_list[index_counter]
         for nucleobase in sequence: # Iterates through the element which index value equals the value of index_counter
-            if nucleobase == "a": 
-                empty_dictionary["a"] += 1
-            elif nucleobase == "t":
-                empty_dictionary["t"] += 1
-            elif nucleobase == "c":
-                empty_dictionary["c"] += 1
-            elif nucleobase == "g":
-                empty_dictionary["g"] += 1
+            if nucleobase in empty_dictionary:
+                empty_dictionary[nucleobase] += 1
             else:
                 continue
-        
         letters = list(empty_dictionary.keys()) # Creates list variable with dictionary keys as elements
         frequency = list(empty_dictionary.values()) # Creates list variable with dictionary values as elements
+        plt.figure()
         plt.bar(range(len(empty_dictionary)), # Initiates bar graph using "plt.bar", along with necessary parameters 
                 frequency, 
                 tick_label=letters) 
         plt.title(f"Sequence: {index_counter + 1}")
         plt.xlabel("Nucleobases")
         plt.ylabel("Occurences")
+        plt.savefig(f"sequence_{index_counter + 1}.png")
         plt.show()
+        plt.close()
         index_counter = index_counter + 1 # Changes the value of index_counter and thusly the referred sequence
+
+nucleobase_count("dna_raw.txt")
